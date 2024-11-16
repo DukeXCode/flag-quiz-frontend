e56cd44d88e776bb1f2b1f494a3b71e4468a5eee
Duke <naim.giger@outlook.com>
2024-11-16 16:43:57 +0100

    fix deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 
 export const environment = {
   production: false,
-  supabaseUrl: process.env['SUPABASE_URL'],
-  supabaseKey: process.env['SUPABASE_KEY'],
+  supabaseUrl: process.env['SUPABASE_URL'] || '',
+  supabaseKey: process.env['SUPABASE_KEY']  || '',
 }
