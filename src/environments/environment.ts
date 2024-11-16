6cd0cb4cad2f6861b042be5ffcde17cebfdf10cc
Duke <naim.giger@outlook.com>
2024-11-16 17:03:40 +0100

    fix deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
-// Set URL and API Key and rename to environment.ts
-
 export const environment = {
   production: false,
-  supabaseUrl: process.env['SUPABASE_URL'] || '',
-  supabaseKey: process.env['SUPABASE_KEY']  || '',
+  supabaseUrl: '%SUPABASE_URL%',
+  supabaseKey: '%SUPABASE_KEY%',
 }
